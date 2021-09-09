f43ff033b4882207f5c5b83e418e914a933b6c12
norply <75418405+norply@users.noreply.github.com>
2021-09-09 18:13:01 +0500

    Ввод
    Подключаем русский язык, запрашиваем ввод первого и второго числа.

--- InputOutput.cpp
+++ InputOutput.cpp
@@ -8,6 +8,10 @@ float difference;
 float average;
 int main()
 {
-
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите первое число: ";
+    cin >> number1;
+    cout <<endl << "Введите второе число: ";
+    cin >> number2;
     return(0);
 }
