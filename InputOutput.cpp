95f2ecfeca049cf4b558fb3032419250208f2966
norply <75418405+norply@users.noreply.github.com>
2021-09-09 18:26:31 +0500

    Произведение
    Объявляем переменную для произведения чисел, вычисляем произведение, выводим результат.

--- InputOutput.cpp
+++ InputOutput.cpp
@@ -7,6 +7,7 @@ float number2;
 float sum;
 float difference;
 float average;
+float product;
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -23,6 +24,9 @@ int main()
     
     average = (abs(number1) + abs(number2)) / 2;
     cout << endl << endl << "Среднее арифметическое модулей чисел равно: "<<average;
+    
+    product = number1 * number2;
+    cout << endl << endl << "Произведение чисел равно: "<< product;
 
     return(0);
 }
